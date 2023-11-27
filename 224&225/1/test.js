311f69a4c9996c8c85092c3afc03c3b762e4548b
YcSaber <499071541@qq.com>
2023-11-27 22:17:22 +0800

    224&225 w1-2

---
+++ 224&225/1/test.js
@@ -0,0 +1,30 @@
+// // 输出语句
+// alert('外链式');
+// // alert('wailianshi!');
+// console.log('在控制台中输出信息');
+//
+// // 输入语句
+// prompt('这是一个输入框');
+
+
+
+// // 声明变量
+// var className;
+// // 变量赋值
+// className = 224225;
+// 变量初始化
+// var className = 224225;
+//
+// alert(className);
+// console.log(className);
+
+
+var myName = '小明';
+var address = '黑龙江省牡丹江市';
+var age = 18.5;
+var email = 'xiaoming@qq.com';
+console.log(myName);
+console.log(address);
+console.log(age);
+console.log(email);
+

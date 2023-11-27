c277785b01cfb71556c23b15a008d205ca88eb92
YcSaber <499071541@qq.com>
2023-11-27 22:16:07 +0800

    221 w1-2

---
+++ 221/1/test.js
@@ -0,0 +1,72 @@
+// alert('外链式！');
+//
+// // 输出语句
+// alert('这是一个警告框');
+// console.log('浏览器控制台输出');
+// // 输入语句
+// prompt('浏览器输入框');
+
+
+// // 声明变量
+// var age;
+// // 变量赋值
+// age = 18;
+// // 变量初始化
+// var age = 19;
+
+
+
+// // 变量的应用
+// var myName = '小明';
+// var age = 18;
+// var address = '黑龙江省牡丹江市';
+// var score = 81.5;
+// var email = 'xiaoming@163.com';
+//
+// console.log(myName);
+// console.log(age);
+// console.log(address);
+// console.log(score);
+// console.log(email);
+
+
+// var myName = prompt('请输入您的姓名');
+// alert('您的姓名是：' + myName);
+
+// // 更新覆盖变量值
+// var address = '黑龙江省牡丹江市爱民区';
+// console.log(address);
+// address = '黑龙江省牡丹江市西安区';
+// console.log(address);
+
+//
+// // 同时声明多个变量，不赋值
+// var myName,age,score;
+// myName = '张三';
+// age = 19;
+// score =88.5;
+// console.log(myName);
+// console.log(age);
+// console.log(score);
+
+// // 同时声明多个变量，并直接赋值
+// var myName = '小明',
+//     age = 19,
+//     address = '黑龙江省牡丹江市';
+// console.log(myName);
+// console.log(age);
+// console.log(address);
+//
+// // 声明不赋值
+// var age;
+// console.log(age);
+
+
+
+
+
+
+
+
+
+
